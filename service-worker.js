9d2d941c8da967d43b4ab1edf0c90030375f90e4
Maksym Titenko <Titenko.m@gmail.com>
2025-07-15 01:31:21 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,76 +1,92 @@
----
-layout: null
----
-
 const CACHE_NAME = 'jekyll-garden-v1';
 const urlsToCache = [
-  '{{ site.baseurl }}/',
-  '{{ site.baseurl }}/index.html',
-  '{{ site.baseurl }}/about.html',
-  '{{ site.baseurl }}/blog.html',
-  '{{ site.baseurl }}/credits.html',
-  '{{ site.baseurl }}/notes.html',
-  '{{ site.baseurl }}/404.html',
-  '{{ site.baseurl }}/feed.xml',
-  '{{ site.baseurl }}/sitemap.xml',
-  '{{ site.baseurl }}/robots.txt',
-  '{{ site.baseurl }}/SearchData.json',
-  '{{ site.baseurl }}/autocomplete.txt',
+  '/jekyll-digital-garden/',
+  '/jekyll-digital-garden/index.html',
+  '/jekyll-digital-garden/about.html',
+  '/jekyll-digital-garden/blog.html',
+  '/jekyll-digital-garden/credits.html',
+  '/jekyll-digital-garden/notes.html',
+  '/jekyll-digital-garden/404.html',
+  '/jekyll-digital-garden/feed.xml',
+  '/jekyll-digital-garden/sitemap.xml',
+  '/jekyll-digital-garden/robots.txt',
+  '/jekyll-digital-garden/SearchData.json',
+  '/jekyll-digital-garden/autocomplete.txt',
 
   // CSS
-  '{{ site.baseurl }}/assets/css/style.css',
-  '{{ site.baseurl }}/assets/css/style2.css',
-  '{{ site.baseurl }}/assets/css/header.css',
-  '{{ site.baseurl }}/assets/css/fruity.css',
-  '{{ site.baseurl }}/assets/css/vendor/Katex.css',
+  '/jekyll-digital-garden/assets/css/style.css',
+  '/jekyll-digital-garden/assets/css/style2.css',
+  '/jekyll-digital-garden/assets/css/header.css',
+  '/jekyll-digital-garden/assets/css/fruity.css',
+  '/jekyll-digital-garden/assets/css/vendor/Katex.css',
 
   // JS
-  '{{ site.baseurl }}/assets/js/Search.js',
+  '/jekyll-digital-garden/assets/js/Search.js',
+  '/jekyll-digital-garden/assets/js/vendor/lunr.min.js',
 
-  // Изображения
-  {% for file in site.static_files %}
-    {% if file.path contains '/assets/img/' %}
-      '{{ site.baseurl }}{{ file.path }}',
-    {% endif %}
-  {% endfor %}
+  // Иконки и изображения
+  '/jekyll-digital-garden/assets/img/1-how-to.png',
+  '/jekyll-digital-garden/assets/img/2-how-to.png',
+  '/jekyll-digital-garden/assets/img/3-how-to.png',
+  '/jekyll-digital-garden/assets/img/4-how-to.png',
+  '/jekyll-digital-garden/assets/img/5-how-to.png',
+  '/jekyll-digital-garden/assets/img/6-how-to.png',
+  '/jekyll-digital-garden/assets/img/7-how-to.png',
+  '/jekyll-digital-garden/assets/img/OGImg.png',
+  '/jekyll-digital-garden/assets/img/Screenshot-Jekyll-Garden-Menu.png',
+  '/jekyll-digital-garden/assets/img/Screenshot-Jekyll-Garden-New-Menu.png',
+  '/jekyll-digital-garden/assets/img/Screenshot-Jekyll-Garden-New-Search.png',
+  '/jekyll-digital-garden/assets/img/Screenshot-Jekyll-Garden-New.png',
+  '/jekyll-digital-garden/assets/img/Screenshot-Jekyll-Garden.png',
+  '/jekyll-digital-garden/assets/img/external-link.svg',
+  '/jekyll-digital-garden/assets/img/favicon.ico',
+  '/jekyll-digital-garden/assets/img/favicon.svg',
+  '/jekyll-digital-garden/assets/img/icon-192.svg',
+  '/jekyll-digital-garden/assets/img/icon-512.svg',
+  '/jekyll-digital-garden/assets/img/icon.png',
+  '/jekyll-digital-garden/assets/img/icon2.png',
+  '/jekyll-digital-garden/assets/img/moon.svg',
+  '/jekyll-digital-garden/assets/img/sun.svg',
+  '/jekyll-digital-garden/assets/img/og-image.svg',
 
-  // Шрифты KaTeX
-  {% assign katex_fonts = "KaTeX_AMS-Regular,KaTeX_Caligraphic-Bold,KaTeX_Caligraphic-Regular,KaTeX_Fraktur-Bold,KaTeX_Fraktur-Regular,KaTeX_Main-BoldItalic,KaTeX_Main-Bold,KaTeX_Main-Italic,KaTeX_Main-Regular,KaTeX_Math-BoldItalic,KaTeX_Math-Italic,KaTeX_SansSerif-Bold,KaTeX_SansSerif-Italic,KaTeX_SansSerif-Regular,KaTeX_Script-Regular,KaTeX_Size1-Regular,KaTeX_Size2-Regular,KaTeX_Size3-Regular,KaTeX_Size4-Regular,KaTeX_Typewriter-Regular" | split: "," %}
-  {% for font in katex_fonts %}
-    '{{ site.baseurl }}/assets/css/vendor/fonts/{{ font }}.ttf',
-    '{{ site.baseurl }}/assets/css/vendor/fonts/{{ font }}.woff',
-    '{{ site.baseurl }}/assets/css/vendor/fonts/{{ font }}.woff2',
-  {% endfor %}
+  // KaTeX шрифты
+  '/jekyll-digital-garden/assets/css/vendor/fonts/KaTeX_AMS-Regular.woff2',
+  '/jekyll-digital-garden/assets/css/vendor/fonts/KaTeX_Caligraphic-Bold.woff2',
+  '/jekyll-digital-garden/assets/css/vendor/fonts/KaTeX_Caligraphic-Regular.woff2',
+  '/jekyll-digital-garden/assets/css/vendor/fonts/KaTeX_Fraktur-Bold.woff2',
+  '/jekyll-digital-garden/assets/css/vendor/fonts/KaTeX_Fraktur-Regular.woff2',
+  '/jekyll-digital-garden/assets/css/vendor/fonts/KaTeX_Main-BoldItalic.woff2',
+  '/jekyll-digital-garden/assets/css/vendor/fonts/KaTeX_Main-Bold.woff2',
+  '/jekyll-digital-garden/assets/css/vendor/fonts/KaTeX_Main-Italic.woff2',
+  '/jekyll-digital-garden/assets/css/vendor/fonts/KaTeX_Main-Regular.woff2',
+  '/jekyll-digital-garden/assets/css/vendor/fonts/KaTeX_Math-BoldItalic.woff2',
+  '/jekyll-digital-garden/assets/css/vendor/fonts/KaTeX_Math-Italic.woff2',
+  '/jekyll-digital-garden/assets/css/vendor/fonts/KaTeX_SansSerif-Bold.woff2',
+  '/jekyll-digital-garden/assets/css/vendor/fonts/KaTeX_SansSerif-Italic.woff2',
+  '/jekyll-digital-garden/assets/css/vendor/fonts/KaTeX_SansSerif-Regular.woff2',
+  '/jekyll-digital-garden/assets/css/vendor/fonts/KaTeX_Script-Regular.woff2',
+  '/jekyll-digital-garden/assets/css/vendor/fonts/KaTeX_Size1-Regular.woff2',
+  '/jekyll-digital-garden/assets/css/vendor/fonts/KaTeX_Size2-Regular.woff2',
+  '/jekyll-digital-garden/assets/css/vendor/fonts/KaTeX_Size3-Regular.woff2',
+  '/jekyll-digital-garden/assets/css/vendor/fonts/KaTeX_Size4-Regular.woff2',
+  '/jekyll-digital-garden/assets/css/vendor/fonts/KaTeX_Typewriter-Regular.woff2',
 
-  // Иконки
-  '{{ site.baseurl }}/assets/img/favicon.ico',
-  '{{ site.baseurl }}/assets/img/favicon.svg',
-  '{{ site.baseurl }}/assets/img/icon.png',
-  '{{ site.baseurl }}/assets/img/icon-192.svg',
-  '{{ site.baseurl }}/assets/img/icon-512.svg',
-  '{{ site.baseurl }}/manifest.json',
-  '{{ site.baseurl }}/manifest.webmanifest'
+  // PWA манифест
+  '/jekyll-digital-garden/manifest.json',
+  '/jekyll-digital-garden/manifest.webmanifest'
 ];
 
-// Установка Service Worker и кэширование
+// Установка Service Worker
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(urlsToCache);
     })
   );
+  self.skipWaiting(); // немедленная активация нового SW
 });
 
-// Обработка fetch-запросов
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
-
-// Очистка устаревшего кэша
+// Активация и удаление старых кэшей
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(cacheNames =>
@@ -82,5 +98,13 @@ self.addEventListener('activate', event => {
         })
       )
     )
+  );
+  self.clients.claim(); // взять управление активными страницами
+});
+
+// Обработка запросов: сначала сеть, потом кэш
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
